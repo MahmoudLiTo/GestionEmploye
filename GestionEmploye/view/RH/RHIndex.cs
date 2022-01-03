7de6c668f656acf215081103bbed03357bfd31f8
mahmoud <louarmahmoud@gmail.com>
2022-01-03 16:56:42 +0100

    hh

---
+++ GestionEmploye/view/RH/RHIndex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEmploye.view.RH
+{
+    public partial class RHIndex : Form
+    {
+        public RHIndex()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GestionEmploye/view/employe/indexEmploye.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionEmploye.view.employe
+{
+    public partial class indexEmploye : Form
+    {
+        public indexEmploye()
+        {
+            InitializeComponent();
+        }
+    }
+}
